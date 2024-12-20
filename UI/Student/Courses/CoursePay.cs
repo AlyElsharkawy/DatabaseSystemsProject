34e5dc374cfcacb2b39a7a959a637e724eb6ef4c
BelalSalem <62998842+TheBombh2@users.noreply.github.com>
2024-12-20 18:28:39 +0200

    CourseEnroll and CoursePay

---
+++ UI/Student/Courses/CoursePay.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DatabaseSystemsProject.UI.Student.Courses
+{
+	public partial class CoursePay : Form
+	{
+		public CoursePay()
+		{
+			InitializeComponent();
+		}
+	}
+}
