a1b83b19e0ac999b7693308d65ce5607c50b8678
BelalSalem <62998842+TheBombh2@users.noreply.github.com>
2024-12-21 19:40:20 +0200

    Video

--- UI/Instructor/Course/Creation/Subsections/AddVideo.Designer.cs
+++ UI/Instructor/Course/Creation/Subsections/AddVideo.Designer.cs
@@ -28,12 +28,97 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			this.button1 = new System.Windows.Forms.Button();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.button2 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+			this.button1.Location = new System.Drawing.Point(95, 72);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(125, 33);
+			this.button1.TabIndex = 11;
+			this.button1.Text = "Select";
+			this.button1.UseVisualStyleBackColor = true;
+			// 
+			// textBox1
+			// 
+			this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+			this.textBox1.Location = new System.Drawing.Point(70, 12);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(573, 32);
+			this.textBox1.TabIndex = 10;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+			this.label2.Location = new System.Drawing.Point(12, 15);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(52, 26);
+			this.label2.TabIndex = 9;
+			this.label2.Text = "Title";
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+			this.label1.Location = new System.Drawing.Point(12, 79);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(68, 26);
+			this.label1.TabIndex = 12;
+			this.label1.Text = "Video";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+			this.label3.Location = new System.Drawing.Point(236, 79);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(149, 26);
+			this.label3.TabIndex = 13;
+			this.label3.Text = "Path/To/Video";
+			// 
+			// button2
+			// 
+			this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+			this.button2.Location = new System.Drawing.Point(518, 156);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(125, 33);
+			this.button2.TabIndex = 14;
+			this.button2.Text = "Submit";
+			this.button2.UseVisualStyleBackColor = true;
+			// 
+			// AddVideo
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.ClientSize = new System.Drawing.Size(668, 205);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.label2);
+			this.Name = "AddVideo";
 			this.Text = "AddVideo";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Button button2;
 	}
 }
