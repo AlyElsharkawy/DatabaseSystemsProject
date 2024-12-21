526a10e24ca78a851350b3fe82bce342bbed391d
BelalSalem <62998842+TheBombh2@users.noreply.github.com>
2024-12-21 11:59:21 +0200

    ModuleEdit

---
+++ UI/Instructor/Course/Creation/ModulesEdit.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DatabaseSystemsProject.UI.Instructor.Course.Creation
+{
+	public partial class ModulesEdit : Form
+	{
+		public ModulesEdit()
+		{
+			InitializeComponent();
+		}
+	}
+}
