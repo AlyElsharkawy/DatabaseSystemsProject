4b727098c3cb0c6390b5ab69c2b1e2388d1552f2
BelalSalem <62998842+TheBombh2@users.noreply.github.com>
2024-12-21 19:36:29 +0200

    reading and assign

--- UI/Instructor/Course/Creation/Subsections/AddAssign.Designer.cs
+++ UI/Instructor/Course/Creation/Subsections/AddAssign.Designer.cs
@@ -28,12 +28,85 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			this.label1 = new System.Windows.Forms.Label();
+			this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+			this.button1 = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+			this.label1.Location = new System.Drawing.Point(12, 9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(83, 26);
+			this.label1.TabIndex = 2;
+			this.label1.Text = "Prompt";
+			// 
+			// richTextBox1
+			// 
+			this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+			this.richTextBox1.Location = new System.Drawing.Point(101, 9);
+			this.richTextBox1.Name = "richTextBox1";
+			this.richTextBox1.Size = new System.Drawing.Size(573, 361);
+			this.richTextBox1.TabIndex = 3;
+			this.richTextBox1.Text = "";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+			this.label2.Location = new System.Drawing.Point(12, 393);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(119, 26);
+			this.label2.TabIndex = 4;
+			this.label2.Text = "Max Grade";
+			// 
+			// numericUpDown1
+			// 
+			this.numericUpDown1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+			this.numericUpDown1.Location = new System.Drawing.Point(137, 387);
+			this.numericUpDown1.Name = "numericUpDown1";
+			this.numericUpDown1.Size = new System.Drawing.Size(120, 32);
+			this.numericUpDown1.TabIndex = 5;
+			// 
+			// button1
+			// 
+			this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+			this.button1.Location = new System.Drawing.Point(525, 387);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(149, 35);
+			this.button1.TabIndex = 6;
+			this.button1.Text = "Submit";
+			this.button1.UseVisualStyleBackColor = true;
+			// 
+			// AddAssign
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.ClientSize = new System.Drawing.Size(694, 450);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.numericUpDown1);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.richTextBox1);
+			this.Controls.Add(this.label1);
+			this.Name = "AddAssign";
 			this.Text = "AddAssign";
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.RichTextBox richTextBox1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.NumericUpDown numericUpDown1;
+		private System.Windows.Forms.Button button1;
 	}
 }
--- UI/Instructor/Course/Creation/Subsections/AddReading.Designer.cs
+++ UI/Instructor/Course/Creation/Subsections/AddReading.Designer.cs
@@ -28,12 +28,83 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// richTextBox1
+			// 
+			this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+			this.richTextBox1.Location = new System.Drawing.Point(106, 55);
+			this.richTextBox1.Name = "richTextBox1";
+			this.richTextBox1.Size = new System.Drawing.Size(573, 361);
+			this.richTextBox1.TabIndex = 5;
+			this.richTextBox1.Text = "";
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+			this.label1.Location = new System.Drawing.Point(12, 55);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(88, 26);
+			this.label1.TabIndex = 4;
+			this.label1.Text = "Content";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+			this.label2.Location = new System.Drawing.Point(12, 9);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(52, 26);
+			this.label2.TabIndex = 6;
+			this.label2.Text = "Title";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+			this.textBox1.Location = new System.Drawing.Point(106, 6);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(573, 32);
+			this.textBox1.TabIndex = 7;
+			// 
+			// button1
+			// 
+			this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+			this.button1.Location = new System.Drawing.Point(307, 433);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(149, 35);
+			this.button1.TabIndex = 8;
+			this.button1.Text = "Submit";
+			this.button1.UseVisualStyleBackColor = true;
+			// 
+			// AddReading
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.ClientSize = new System.Drawing.Size(702, 480);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.richTextBox1);
+			this.Controls.Add(this.label1);
+			this.Name = "AddReading";
 			this.Text = "AddReading";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.RichTextBox richTextBox1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Button button1;
 	}
 }
