6544a3c38b65d56786f6d90f673dfe1deeba4c7c
BelalSalem <62998842+TheBombh2@users.noreply.github.com>
2024-12-21 19:30:18 +0200

    Module

--- UI/Instructor/Course/Creation/CreateModule.Designer.cs
+++ UI/Instructor/Course/Creation/CreateModule.Designer.cs
@@ -28,12 +28,58 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// textBox1
+			// 
+			this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+			this.textBox1.Location = new System.Drawing.Point(166, 6);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(288, 32);
+			this.textBox1.TabIndex = 0;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+			this.label1.Location = new System.Drawing.Point(12, 9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(148, 26);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "Module Name";
+			// 
+			// button1
+			// 
+			this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+			this.button1.Location = new System.Drawing.Point(17, 58);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(437, 45);
+			this.button1.TabIndex = 2;
+			this.button1.Text = "Submit";
+			this.button1.UseVisualStyleBackColor = true;
+			// 
+			// CreateModule
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.ClientSize = new System.Drawing.Size(471, 118);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.textBox1);
+			this.Name = "CreateModule";
 			this.Text = "CreateModule";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button button1;
 	}
 }
