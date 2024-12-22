315078b2ccf72808759bff652688e66df445f5ce
BelalSalem <62998842+TheBombh2@users.noreply.github.com>
2024-12-22 18:25:01 +0200

    login

--- Utility/PasswordEncryption.cs
+++ Utility/PasswordEncryption.cs
@@ -51,5 +51,20 @@ namespace DatabaseSystemsProject.Utility
 			return byteArray;
 		}
 
+
+		public static bool CompareByteArrays(byte[] array1, byte[] array2)
+		{
+			if (array1.Length != array2.Length)
+				return false;
+
+			for (int i = 0; i < array1.Length; i++)
+			{
+				if (array1[i] != array2[i])
+					return false;
+			}
+			return true;
+		}
+
+
 	}
 }
