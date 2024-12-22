c2247b8ad441ab406821acd7479a73f56cd3ac1c
BelalSalem <62998842+TheBombh2@users.noreply.github.com>
2024-12-22 14:44:51 +0200

    addInstructor

---
+++ UI/Admin/AddInstructor.Designer.cs
@@ -0,0 +1,298 @@
+﻿namespace DatabaseSystemsProject.UI.Admin
+{
+	partial class AddInstructor
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.pfpPathLBL = new System.Windows.Forms.Label();
+			this.button3 = new System.Windows.Forms.Button();
+			this.label7 = new System.Windows.Forms.Label();
+			this.textBox5 = new System.Windows.Forms.TextBox();
+			this.label6 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label1 = new System.Windows.Forms.Label();
+			this.button2 = new System.Windows.Forms.Button();
+			this.selectPfpBTN = new System.Windows.Forms.Button();
+			this.textBox4 = new System.Windows.Forms.TextBox();
+			this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.label8 = new System.Windows.Forms.Label();
+			this.label9 = new System.Windows.Forms.Label();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.SuspendLayout();
+			// 
+			// pfpPathLBL
+			// 
+			this.pfpPathLBL.AutoSize = true;
+			this.pfpPathLBL.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F);
+			this.pfpPathLBL.Location = new System.Drawing.Point(234, 140);
+			this.pfpPathLBL.Name = "pfpPathLBL";
+			this.pfpPathLBL.Size = new System.Drawing.Size(29, 13);
+			this.pfpPathLBL.TabIndex = 34;
+			this.pfpPathLBL.Text = "Path";
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(278, 308);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(75, 23);
+			this.button3.TabIndex = 33;
+			this.button3.Text = "Cacnel";
+			this.button3.UseVisualStyleBackColor = true;
+			// 
+			// label7
+			// 
+			this.label7.AutoSize = true;
+			this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+			this.label7.Location = new System.Drawing.Point(10, 235);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(137, 20);
+			this.label7.TabIndex = 32;
+			this.label7.Text = "Confirm Password";
+			// 
+			// textBox5
+			// 
+			this.textBox5.Location = new System.Drawing.Point(153, 201);
+			this.textBox5.Name = "textBox5";
+			this.textBox5.PasswordChar = '*';
+			this.textBox5.Size = new System.Drawing.Size(200, 20);
+			this.textBox5.TabIndex = 31;
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+			this.label6.Location = new System.Drawing.Point(10, 199);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(78, 20);
+			this.label6.TabIndex = 30;
+			this.label6.Text = "Password";
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+			this.label5.Location = new System.Drawing.Point(10, 138);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(106, 20);
+			this.label5.TabIndex = 29;
+			this.label5.Text = "Profile Picture";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+			this.label4.Location = new System.Drawing.Point(10, 106);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(48, 20);
+			this.label4.TabIndex = 28;
+			this.label4.Text = "Email";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+			this.label3.Location = new System.Drawing.Point(10, 74);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(99, 20);
+			this.label3.TabIndex = 27;
+			this.label3.Text = "Date of Birth";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+			this.label2.Location = new System.Drawing.Point(10, 42);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(48, 20);
+			this.label2.TabIndex = 26;
+			this.label2.Text = "Email";
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+			this.label1.Location = new System.Drawing.Point(10, 13);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(51, 20);
+			this.label1.TabIndex = 25;
+			this.label1.Text = "Name";
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(153, 308);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(75, 23);
+			this.button2.TabIndex = 24;
+			this.button2.Text = "Register";
+			this.button2.UseVisualStyleBackColor = true;
+			// 
+			// selectPfpBTN
+			// 
+			this.selectPfpBTN.Location = new System.Drawing.Point(153, 135);
+			this.selectPfpBTN.Name = "selectPfpBTN";
+			this.selectPfpBTN.Size = new System.Drawing.Size(75, 23);
+			this.selectPfpBTN.TabIndex = 23;
+			this.selectPfpBTN.Text = "Select";
+			this.selectPfpBTN.UseVisualStyleBackColor = true;
+			// 
+			// textBox4
+			// 
+			this.textBox4.Location = new System.Drawing.Point(153, 172);
+			this.textBox4.Name = "textBox4";
+			this.textBox4.PasswordChar = '*';
+			this.textBox4.Size = new System.Drawing.Size(200, 20);
+			this.textBox4.TabIndex = 22;
+			// 
+			// dateTimePicker1
+			// 
+			this.dateTimePicker1.Location = new System.Drawing.Point(153, 74);
+			this.dateTimePicker1.Name = "dateTimePicker1";
+			this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+			this.dateTimePicker1.TabIndex = 21;
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(153, 108);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(200, 20);
+			this.textBox2.TabIndex = 20;
+			// 
+			// maskedTextBox1
+			// 
+			this.maskedTextBox1.Location = new System.Drawing.Point(153, 12);
+			this.maskedTextBox1.Name = "maskedTextBox1";
+			this.maskedTextBox1.Size = new System.Drawing.Size(200, 20);
+			this.maskedTextBox1.TabIndex = 19;
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(153, 44);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(200, 20);
+			this.textBox1.TabIndex = 18;
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(153, 237);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.PasswordChar = '*';
+			this.textBox3.Size = new System.Drawing.Size(200, 20);
+			this.textBox3.TabIndex = 35;
+			// 
+			// label8
+			// 
+			this.label8.AutoSize = true;
+			this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+			this.label8.Location = new System.Drawing.Point(10, 172);
+			this.label8.Name = "label8";
+			this.label8.Size = new System.Drawing.Size(113, 20);
+			this.label8.TabIndex = 36;
+			this.label8.Text = "Phone number";
+			// 
+			// label9
+			// 
+			this.label9.AutoSize = true;
+			this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+			this.label9.Location = new System.Drawing.Point(10, 270);
+			this.label9.Name = "label9";
+			this.label9.Size = new System.Drawing.Size(99, 20);
+			this.label9.TabIndex = 37;
+			this.label9.Text = "Organization";
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Location = new System.Drawing.Point(153, 272);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(200, 21);
+			this.comboBox1.TabIndex = 38;
+			// 
+			// AddInstructor
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(369, 343);
+			this.Controls.Add(this.comboBox1);
+			this.Controls.Add(this.label9);
+			this.Controls.Add(this.label8);
+			this.Controls.Add(this.textBox3);
+			this.Controls.Add(this.pfpPathLBL);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.label7);
+			this.Controls.Add(this.textBox5);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.selectPfpBTN);
+			this.Controls.Add(this.textBox4);
+			this.Controls.Add(this.dateTimePicker1);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.maskedTextBox1);
+			this.Controls.Add(this.textBox1);
+			this.Name = "AddInstructor";
+			this.Text = "AddInstructor";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label pfpPathLBL;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.TextBox textBox5;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button selectPfpBTN;
+		private System.Windows.Forms.TextBox textBox4;
+		private System.Windows.Forms.DateTimePicker dateTimePicker1;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.Label label8;
+		private System.Windows.Forms.Label label9;
+		private System.Windows.Forms.ComboBox comboBox1;
+	}
+}
